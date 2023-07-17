9305220468b8cf3e264b2f7b89a6719610da0cae
Sheila-nk <sheilankw@gmail.com>
2023-07-17 21:53:55 +0300

    add ini configuration

--- pytest_plugin_skeleton/plugin.py
+++ pytest_plugin_skeleton/plugin.py
@@ -4,3 +4,10 @@ def pytest_addoption(parser):
         action="store_true",
         help="New cli argument that does something"
         )
+    
+    parser.addini(
+        "doctest_optionflags",
+        "Option flags for doctests",
+        type="args",
+        default=["NORMALIZE_WHITESPACE", "ELLIPSIS", "IGNORE_EXCEPTION_DETAIL"]
+        )
