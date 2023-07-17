ab9844600b936d7e826ef96dfe2869e504f3b397
Sheila-nk <sheilankw@gmail.com>
2023-07-17 21:40:24 +0300

    remove pytest hook

--- pytest_plugin_skeleton/plugin.py
+++ pytest_plugin_skeleton/plugin.py
@@ -4,8 +4,3 @@ def pytest_addoption(parser):
         action="store_true",
         help="New cli argument that does something"
         )
-
-def pytest_configure(config):
-    new_cli_arg = config.getoption("--new-cli-arg")
-    if not new_cli_arg:
-        return
